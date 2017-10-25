c8baeb30cb4edf870c8222bc3d0fa9fcf9d7a644
alumno <alumno@DESKTOP-CNRL87V>
2017-10-25 11:39:58 -0300

    Agrego de Getter y Setter

--- src/Empleado2/Empleado.java
+++ src/Empleado2/Empleado.java
@@ -15,10 +15,67 @@ public class Empleado {
     private int legajo; //privado ya que son propiedades
     private String ape;
     private String nom;
+    private String cate;
     private double SBasico;
     private LocalDate FIngreso;
     
     private Sueldo MiSueldo;
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApe() {
+        return ape;
+    }
+
+    public void setApe(String ape) {
+        this.ape = ape;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCate() {
+        return cate;
+    }
+
+    public void setCate(String cate) {
+        this.cate = cate;
+    }
+
+    public double getSBasico() {
+        return SBasico;
+    }
+
+    public void setSBasico(double SBasico) {
+        this.SBasico = SBasico;
+    }
+
+    public LocalDate getFIngreso() {
+        return FIngreso;
+    }
+
+    public void setFIngreso(LocalDate FIngreso) {
+        this.FIngreso = FIngreso;
+    }
+
+    public Sueldo getMiSueldo() {
+        return MiSueldo;
+    }
+
+    public void setMiSueldo(Sueldo MiSueldo) {
+        this.MiSueldo = MiSueldo;
+    }
     
     public Empleado()
     {
